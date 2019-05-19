2d6a9d3db0d9a9ac2334865b4f2206f4a1184eea
MelvinMuuo <muuomelvin73@gmail.com>
2019-05-19 09:13:39 +0300

    add heros.vtl for disaplying list of heros

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,5 +20,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("heros", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("heros", Hero.all());
+      model.put("template", "templates/heros.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
   }
 }
